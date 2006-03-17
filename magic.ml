a0dd3a4fea0d3c5e9c9199e0cbaa4840a60d1a5b
chris_77 <chris_77>
2006-03-17 10:00:33 +0000

    Better ':' escaping; buffer has an optional argument

--- magic.ml
+++ magic.ml
@@ -16,7 +16,7 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
    LICENSE for more details.
 *)
-(* 	$Id: magic.ml,v 1.1 2005/01/24 17:28:38 chris_77 Exp $	 *)
+(* 	$Id: magic.ml,v 1.2 2006/03/17 10:00:33 chris_77 Exp $	 *)
 
 
 
@@ -25,6 +25,18 @@ exception Failure of string
 let () = Callback.register_exception "Magic.Failure" (Failure "message")
 
 type t (* hold magic_t *)
+
+external magic_open : int -> t = "ocaml_magic_open"
+external close : t -> unit = "ocaml_magic_close"
+external magic_file : t -> string -> string = "ocaml_magic_file"
+external magic_buffer : t -> string -> int -> string = "ocaml_magic_buffer"
+external magic_setflags : t -> int -> unit = "ocaml_magic_setflags"
+external magic_check_default : t -> bool = "ocaml_magic_check_default"
+external magic_check : t -> string -> bool = "ocaml_magic_check"
+external magic_compile_default : t -> unit = "ocaml_magic_compile_default"
+external magic_compile : t -> string -> unit = "ocaml_magic_compile"
+external magic_load_default : t -> unit = "ocaml_magic_load_default"
+external magic_load : t -> string -> unit = "ocaml_magic_load"
 
 type flag =
   | Symlink
@@ -36,7 +48,7 @@ type flag =
   | Preserve_atime
   | Raw
 
-(* Keep in sync with magic.h *)
+(* WARNING: Keep in sync with magic.h *)
 let int_of_flag = function
   | Symlink	-> 0x002
   | Compress	-> 0x004
@@ -51,30 +63,36 @@ let int_of_flags flags =
   List.fold_left (fun fs f -> fs lor (int_of_flag f)) 0x000 flags
 
 
-external magic_open : int -> t = "magic_open_stub"
-external close : t -> unit = "magic_close_stub"
-external file : t -> string -> string = "magic_file_stub"
-external buffer : t -> string -> string = "magic_buffer_stub"
+(* FIXME: is this escaping correct for libmagic??? *)
+let escape_colon s =
+  let len = String.length s in
+  let n = ref 0 in
+  for i = 0 to len - 1 do if s.[i] = ':' then incr n done;
+  if !n = 0 then s else
+    let s' = String.create (len + !n) in
+    let j = ref 0 in
+    for i = 0 to len - 1 do
+      if s.[i] = ':' then (s'.[!j] <- '\\'; incr j);
+      s'.[!j] <- s.[i]; incr j
+    done;
+    s'
 
-external magic_setflags : t -> int -> unit = "magic_setflags_stub"
-external magic_check_default : t -> bool = "magic_check_default_stub"
-external magic_check : t -> string -> bool = "magic_check_stub"
-external magic_compile_default : t -> unit = "magic_compile_default_stub"
-external magic_compile : t -> string -> unit = "magic_compile_stub"
-external magic_load_default : t -> unit = "magic_load_default_stub"
-external magic_load : t -> string -> unit = "magic_load_stub"
+(* Concatenate the filenames with ":".  If ":" is present in a
+   filename, escape it. *)
+let concat filenames =
+  String.concat ":" (List.map escape_colon filenames)
 
 let load cookie = function
   | [] -> magic_load_default cookie
-  | filenames -> magic_load cookie (String.concat ":" filenames)
+  | filenames -> magic_load cookie (concat filenames)
 
 let compile cookie = function
   | [] -> magic_compile_default cookie
-  | filenames -> magic_compile cookie (String.concat ":" filenames)
+  | filenames -> magic_compile cookie (concat filenames)
 
 let check cookie = function
   | [] -> magic_check_default cookie
-  | filenames -> magic_check cookie (String.concat ":" filenames)
+  | filenames -> magic_check cookie (concat filenames)
 
 
 let create ?(flags=[]) filenames =
@@ -84,3 +102,18 @@ let create ?(flags=[]) filenames =
 
 let setflags cookie flags =
   magic_setflags cookie (int_of_flags flags)
+
+let file cookie filename =
+  (* FIXME: For a strange reason the toplevel loops with an error "I/O
+     error: Bad file descriptor" when querying an unexisting file or a
+     char device,...  (this works fine when compiled). *)
+  magic_file cookie filename
+
+let buffer cookie ?len s =
+  let len = match len with
+  | None -> String.length s
+  | Some l ->
+      if 0 <= l && l <= String.length s then l
+      else invalid_arg(Printf.sprintf "Magic.buffer: len=%i not in [0, %i]"
+                         l (String.length s)) in
+  magic_buffer cookie s len
