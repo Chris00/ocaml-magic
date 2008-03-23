79933b41f7bb4372b98aebfe09b1e374e19f3110
chris_77 <chris_77>
2008-03-23 15:56:48 +0000

    - Updated some paths.

--- examples/test.ml
+++ examples/test.ml
@@ -1,4 +1,4 @@
-#directory "/home/trch/Software/OCaml/ocaml-magic";;
+#directory "../src";;
 #load "magic.cma";;
 
 open Printf;;
@@ -6,8 +6,8 @@ open Printf;;
 printf "\n**********OCaml script starts*************************\n%!";;
 let c = Magic.create ["/usr/share/file/magic"];;
 
-let m1 = Magic.file c "dllmagic_stub.so";;
-let m2 = Magic.file c "file.com";;
+let m1 = Magic.file c "../src/dllmagic_stubs.so";;
+let m2 = Magic.file c "file.ml";;
 let m3 = Magic.file c "/dev/null";;
 let m4 = Magic.file c "xxx";;
 
